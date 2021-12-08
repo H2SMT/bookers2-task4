d1272295aab336253b4f0dad9f2ec57b2eb2b715
H2SMT <ykr6g3kx@gmail.com>
2021-12-08 09:30:04 +0000

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
- has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :followings, through: :relationships, source: :followed
@@ -26,6 +26,6 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
-  validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
+  validates :name, uniqueness: true,length: { in: 2..20 }
   validates :introduction, length: {maximum: 50}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  root 'homes#top'
+  root to:'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
-
-  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
-  resource :favorites, only: [:create, :destroy]
-  resources :comments, only: [:create, :destroy]
+  
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
 
   resources :users, only: [:index, :show, :edit, :update] do
-  resource :relationships, only: [:create, :destroy]
-  get 'followings' => 'relationships#followings', as: 'followings'
-  get 'followers' => 'relationships#followers', as: 'followers'
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
   end
+  
 end
